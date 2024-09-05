4498deb6dc5f5912e2151aac3e280c2ee599eef7
Pedro Henrique Salazar <ph.s.salazar@gmail.com>
2024-09-05 20:35:19 -0300

    Primeiro commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ApiCrud.Estudantes;
+
+namespace ApiCrud.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Estudante> Estudantes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Banco.sqlite");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Estudantes/EstudanteRotas.cs
@@ -0,0 +1,72 @@
+﻿using ApiCrud.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Update;
+
+namespace ApiCrud.Estudantes
+{
+    public static class EstudanteRotas
+    {
+        public static void AddRotasEstudantes (this WebApplication app)
+        {
+            // Adicionar Estudante
+            app.MapPost("estudantes", async (AddEstudanteRequest request, AppDbContext context, CancellationToken ct) =>
+            {
+                 var estudanteJaExiste = await context.Estudantes.AnyAsync(estudante => estudante.Nome == request.Nome);
+
+                if (estudanteJaExiste)
+                    return Results.Conflict(request.Nome + " já existe no banco de dados.");
+
+
+                var novoEstudate = new Estudante(request.Nome);
+                await context.Estudantes.AddAsync(novoEstudate, ct);
+                await context.SaveChangesAsync(ct);
+
+                var estudanteRetorno = new EstudanteDto(novoEstudate.Id, novoEstudate.Nome);
+                return Results.Ok(estudanteRetorno);
+            });
+
+            // Obter lista de Estudantes
+            app.MapGet("estudantes", async (AppDbContext context, CancellationToken ct) =>
+            {
+                var estudantes = await context.Estudantes
+                    .Where(estudante => estudante.Ativo)
+                    .Select(estudante => new EstudanteDto(estudante.Id, estudante.Nome))
+                    .ToListAsync(ct);
+                return estudantes;
+            });
+
+            // Atualizar Estudante (Nome)
+            app.MapPut("estudante/{id:guid}", async (Guid id,UpdateEstudanteRequest request, AppDbContext context, CancellationToken ct) =>
+            {
+                var estudante = await context.Estudantes.SingleOrDefaultAsync(estudante => estudante.Id == id);
+
+                // Não encontrado
+                if (estudante == null)
+                    return Results.NotFound();
+
+                // Encontrado e atualizar
+                estudante.AtualizarNome(request.Nome);
+
+                // Salvar modificação
+                await context.SaveChangesAsync(ct);
+
+                return Results.Ok(new EstudanteDto(estudante.Id, estudante.Nome));
+            });
+
+
+            // Deletar/Desativar um estudante
+            app.MapDelete("estudante/{id}", async (Guid id, AppDbContext context, CancellationToken ct) =>
+            {
+                var estudante = await context.Estudantes.SingleOrDefaultAsync(estudante => estudante.Id == id);
+
+                // Não encontrou o estudante para desativar o status
+                if (estudante == null)
+                    return Results.NotFound();
+
+                estudante.DesativarEstudante();
+                await context.SaveChangesAsync(ct); // Salvar
+                return Results.Ok();
+            });
+        }
+    }
+}
